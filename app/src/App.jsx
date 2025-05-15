5008cc485f68e142b69f38fd280ca00bcd7b8734
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-05-15 17:30:43 +0530

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+const App = () => {
+
+  return (
+    <>
+      <MainContainer>
+        <TopContainer>
+          <div className="logo">
+            <img src="/logo.svg" alt="logo" />
+          </div>
+
+          <div className="search">
+            <input placeholder="Search Food" />
+          </div>
+        </TopContainer>
+      </MainContainer>
+    </>
+  );
+};
+
+export default App;
+
+const MainContainer = styled.div``;
+const TopContainer = styled.section``;
